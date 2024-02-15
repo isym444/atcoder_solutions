d687aa415c96c0f07ed57a2347072ca7eb7b28d8
isym444 <samir.yep@gmail.com>
2024-02-15 23:31:00 +0000

    Updated problems solved

---
+++ update_stats_daily.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Navigate to the repository directory
+cd /Users/samir/Desktop/atcoder.jp
+
+# Optional: Add all changes to staging
+git add *
+
+# Optional: Commit changes with a generic message
+git commit -m "Updated problems solved"
+
+# Push changes to the remote repository
+git push
