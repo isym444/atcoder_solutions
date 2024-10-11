2d14212c71a832a8a1bc966446ef8b6792885c3c
isym444 <samir.yep@gmail.com>
2024-10-11 23:31:01 +0100

    Updated problems solved

---
+++ abc197/abc197_c/test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <queue>
+#include <array>
+#include <climits>
+#include <cmath>
+#include <set>
+#include <map>
+#include <bitset>
+#include <deque>
+#include <numeric>
+#include <assert.h>
+#include <cassert>
+#include <unordered_map>
+#include <type_traits> // For std::is_floating_point
+#include <cmath> // For std::ceil
+#include <iomanip>
+#include <unordered_set>
+#include <functional>
+#include <type_traits>
+#include <chrono>
+#include <list>
+using namespace std;
+
+int main(){
+    cout << (1|5) << endl;
+    return 0;
+}
