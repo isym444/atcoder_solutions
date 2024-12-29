22e41c2ddc7bef60b764147093fe1907877fcea7
isym444 <samir.yep@gmail.com>
2024-12-29 23:31:03 +0000

    Updated problems solved

--- abc180/abc180_b/main.cpp
+++ abc180/abc180_b/main.cpp
@@ -1847,42 +1847,7 @@ vector<int> dy_wasd = {0,0,1,-1};
 using mint = modint998244353;
 
 
-auto solve(int N, const std::vector<long long> &x) {
-    /* vis.assign(n+1, false);
-    g.assign(n+1, vector<ll>());
-    wg.assign(n + 1, vector<pair<ll,ll>>());
-    parent.assign(n+1, -1); */
-}
+int main(){
 
-int main() {
-    std::ios::sync_with_stdio(false);
-    setIO("");
-    std::cin.tie(nullptr);
-    // sets precision of output of floating point numbers to x number of decimal places
-    cout << fixed << setprecision(11);
-    unordered_map<long long, int, custom_hash> safe_map;
-    int N;
-    std::cin >> N;
-    std::vector<long long> x(N);
-    REP (i, N) {
-        std::cin >> x[i];
-    }
-    auto ans = solve(N, x);
-    std::cout << d << '\n';
-    std::cout << e << '\n';
-    std::cout << f << '\n';
-
-    /* genprimes(1e5); */
-
-    /* //run the bfs and output order of traversed nodes (for loop is only used for non-connected graphs)
-    for (int i = 0; i < n; i++) {
-        if (!v[i])
-            bfs(i);
-    }
-    
-    //Use for problems where you have to go up,down,left,right. Do x+i & y+j and i&j will test all 4 directions. Do x+i+1 & y+j+1 if 0 indexed
-    wasd(
-        //cout << "Use this for problems where you have to go up, down, left right" << endl;
-    ) */
     return 0;
 }
